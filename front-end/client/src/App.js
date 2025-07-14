7142c22526a331d6b1615bff335c30f5aea5cda3
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-07-14 20:31:51 -0300

    Adicionando CSS na página

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -6,14 +6,33 @@ function App() {
   const [activeTab, setActiveTab] = useState('ask-ai');
 
   const handleTabChange = (tab) => {
-    alert(tab);
+    //alert(tab);
     setActiveTab(tab);
   }
 
   return (
     <div className="App">
-      <button onClick={() => handleTabChange('ask-ai')}>Talk With AI</button>
-      <button onClick={() => handleTabChange('recipe-generator')}>Generate Recipe</button>
+      
+      //Botão de pergunte para o AI
+      <button 
+        className={activeTab === 'ask-ai' ? 'active' : ''}
+        onClick={() => handleTabChange('ask-ai')}>
+        Talk With AI
+      </button>
+
+      //Botão de gerador de receitas
+      <button 
+       className={activeTab === 'recipe-generator' ? 'active' : ''}
+       onClick={() => handleTabChange('recipe-generator')}>
+        Generate Recipes
+      </button>
+
+      <div>
+        {activeTab === 'ask-ai' && <h2>Talk With AI</h2>}
+        {activeTab === 'recipe-generator' && <h2>Generate Recipes</h2>}
+      </div>
+
+
     </div>
   );
 }
